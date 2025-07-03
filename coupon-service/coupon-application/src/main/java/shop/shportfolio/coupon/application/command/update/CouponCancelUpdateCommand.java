180c5c5fe6c8bdfe05e35aac52bd8c2cb71163ed
ziogenorwekh <ziogenorwekh@gmail.com>
2025-07-03 22:52:03 +0900

    null

--- coupon-service/coupon-application/src/main/java/shop/shportfolio/coupon/application/command/update/CouponCancelUpdateCommand.java
+++ coupon-service/coupon-application/src/main/java/shop/shportfolio/coupon/application/command/update/CouponCancelUpdateCommand.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
@@ -13,4 +14,7 @@ public class CouponCancelUpdateCommand {
 
     private UUID userId;
     private UUID couponId;
+    private long cancelAmount;
+    private String paymentKey;
+    private String cancelReason;
 }
--- coupon-service/coupon-application/src/main/java/shop/shportfolio/coupon/application/command/update/CouponCancelUpdateResponse.java
+++ coupon-service/coupon-application/src/main/java/shop/shportfolio/coupon/application/command/update/CouponCancelUpdateResponse.java
@@ -1,5 +1,21 @@
 package shop.shportfolio.coupon.application.command.update;
 
-public class CouponCancelUpdateResponse {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import shop.shportfoilo.coupon.domain.valueobject.PaymentMethod;
+import shop.shportfoilo.coupon.domain.valueobject.PaymentStatus;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@AllArgsConstructor
+public class CouponCancelUpdateResponse {
+    private final UUID couponId;
+    private final String paymentKey;
+    private final String cancelReason;
+    private final Long canceledAmount;
+    private final String couponCode;
+    private final String couponStatus;
+    private final LocalDateTime canceledAt;
 }
